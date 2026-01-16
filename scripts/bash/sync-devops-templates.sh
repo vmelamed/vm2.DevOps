ed64ffb25157be457c38e3df15fd010dd19cf12f
Val Melamed <valo@vtmelamed.com>
2026-01-16 14:12:39 -0500

    cosmetic

--- scripts/bash/sync-devops-templates.sh
+++ scripts/bash/sync-devops-templates.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if ! pushd "$GIT_REPOS"; then
+if ! pushd "$GIT_REPOS" > /dev/null; then
     echo "❌  ERROR: Could not change directory to '\$GIT_REPOS'." >&2
     exit 2
 fi
 cp vm2.DevOps/.github/actions/scripts/_common_*.sh vm2.Templates/templates/AddNewPackage/content/scripts/
 cp vm2.DevOps/scripts/bash/sync-devops-templates.sh vm2.Templates/templates/AddNewPackage/content/scripts/
 chmod u+x vm2.Templates/templates/AddNewPackage/content/scripts/*.sh
-popd || exit 3
+popd > /dev/null || exit 3
