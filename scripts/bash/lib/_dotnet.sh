b42c269586b57ff5d8ff409b6fcb5a04cd450a3d
Val Melamed <valo@vtmelamed.com>
2026-01-28 20:44:43 -0500

    Refactor summarizeDotnetBuild function to streamline build result processing and improve variable handling

--- scripts/bash/lib/_dotnet.sh
+++ scripts/bash/lib/_dotnet.sh
@@ -18,51 +18,37 @@ function summarizeDotnetBuild()
     restoreShopt=$(shopt -p nocasematch)
     shopt -s nocasematch
 
-    local built=false
     local line
     while IFS= read -r line; do
-        if [[ $built == false ]]; then
-            if [[ $line =~ Build\ (succeeded)|(FAILED) ]]; then
-                built=true
-                # shellcheck disable=SC2034
-                build_result="${BASH_REMATCH[1]}${BASH_REMATCH[2]}"
-            fi
-            continue
+        if [[ $line =~ Build\ (succeeded)|(FAILED) ]]; then
+            build_result="${BASH_REMATCH[1]}${BASH_REMATCH[2]}"
         elif [[ -z $warnings_count && $line =~ ([0-9]+)\ Warning ]]; then
             warnings_count=${BASH_REMATCH[1]}
-            continue
         elif [[ -z $errors_count && $line =~ ([0-9]+)\ Error ]]; then
             errors_count=${BASH_REMATCH[1]}
-            continue
-        elif [[ $build_result == FAILED ]]; then
-            # in case of failed build, we stop processing after errors count
-            assembly_version='N/A'
-            file_version='N/A'
-            informational_version='N/A'
-            version='N/A'
-            package_version='N/A'
-            minver_version='N/A'
-            break
         elif [[ -z $assembly_version && $line =~ AssemblyVersion:\ ([[:alnum:][:punct:]]+) ]]; then
             assembly_version=${BASH_REMATCH[1]}
-            continue
         elif [[ -z $file_version && $line =~ FileVersion:\ ([[:alnum:][:punct:]]+) ]]; then
             file_version=${BASH_REMATCH[1]}
-            continue
         elif [[ -z $informational_version && $line =~ InformationalVersion:\ ([[:alnum:][:punct:]]+) ]]; then
             informational_version=${BASH_REMATCH[1]}
-            continue
         elif [[ -z $version && $line =~ Version:\ ([[:alnum:][:punct:]]+) ]]; then
             version=${BASH_REMATCH[1]}
-            continue
         elif [[ -z $package_version && $line =~ PackageVersion:\ ([[:alnum:][:punct:]]+) ]]; then
             package_version=${BASH_REMATCH[1]}
-            continue
         elif [[ -z $minver_version && $line =~ MinVerVersion:\ ([[:alnum:][:punct:]]+) ]]; then
             minver_version=${BASH_REMATCH[1]}
-            continue
         fi
     done
+
+    if [[ $build_result == FAILED ]]; then
+        assembly_version='N/A'
+        file_version='N/A'
+        informational_version='N/A'
+        version='N/A'
+        package_version='N/A'
+        minver_version='N/A'
+    fi
 
     # shellcheck disable=SC2154 # _ignore is referenced but not assigned.
     eval "$restoreShopt" &> "$_ignore"
