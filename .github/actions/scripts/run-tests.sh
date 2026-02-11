592a1439d7620c9e0b952889450dfe75198437b2
Val Melamed <valo@vtmelamed.com>
2026-02-11 18:58:57 -0500

    fix: add support for TEST_VAR in run-tests script and update _test.yaml to set default value

--- .github/actions/scripts/run-tests.sh
+++ .github/actions/scripts/run-tests.sh
@@ -59,6 +59,12 @@ if is_defined_variable REPORTGENERATOR_LICENSE && [[ -n "$REPORTGENERATOR_LICENS
 else
     warning "No ReportGenerator license provided. If you have a license, you can set it via the environment variable REPORTGENERATOR_LICENSE to enable additional features in the generated reports."
 fi
+if is_defined_variable TEST_VAR && [[ -n "$TEST_VAR" ]]; then
+    trace "Test variable provided '${TEST_VAR}' via environment variable."
+else
+    warning "No test variable provided. If you have a test variable, you can set it via the environment variable TEST_VAR to enable additional features in the generated reports."
+fi
+
 exit_if_has_errors
 
 test_dir=$(realpath -e "${test_dir}")                                           # the directory of the test project
