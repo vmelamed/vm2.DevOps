241404e41d8cb066c3ec54e81d407ab5466f1c8b
Val Melamed <valo@vtmelamed.com>
2026-04-09 11:30:33 -0400

    fix: remove memory and exporters options from benchmark execution (rely on benchmark class attributes or program)

--- .github/scripts/run-benchmarks.sh
+++ .github/scripts/run-benchmarks.sh
@@ -132,8 +132,6 @@ trace "Running benchmark tests in project '$benchmark_project' with build config
 if ! execute "${benchmark_exe_path}" \
         --filter '*' \
         --join \
-        --memory \
-        --exporters json markdown \
         --artifacts "$artifacts_dir"; then
     error "Benchmarks failed in project '$benchmark_project'."
     exit 2
