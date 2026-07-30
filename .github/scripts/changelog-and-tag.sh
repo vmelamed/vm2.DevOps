ab343fa3acbc9692364c705fbc917e03fccc46de
Val Melamed <valo@vtmelamed.com>
2026-07-30 19:27:26 -0400

    fix: Comment out git remote set-url command to prevent accidental execution

--- .github/scripts/changelog-and-tag.sh
+++ .github/scripts/changelog-and-tag.sh
@@ -64,7 +64,7 @@ fi
 if $ci; then
     execute git config user.name vmelamed
     execute git config user.email vmelamed@users.noreply.github.com
-    # execute git remote set-url origin "https://x-access-token:${RELEASE_PAT}@github.com/${GITHUB_REPOSITORY}.git"
+      # execute git remote set-url origin "https://x-access-token:${RELEASE_PAT}@github.com/${GITHUB_REPOSITORY}.git"
 fi
 
 # ============================================================================
