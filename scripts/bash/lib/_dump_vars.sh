cbe633b9527b1c2d843621a5ec6a5d8ad2933be0
Val Melamed <valo@vtmelamed.com>
2026-09-17 20:48:09 -0400

    fix: update trace output to remove source and line number for table format

--- scripts/bash/lib/_dump_vars.sh
+++ scripts/bash/lib/_dump_vars.sh
@@ -254,7 +254,7 @@ function dump_vars()
 
     #
     _current_table=$(get_table_format)
-    trace "Using current table: '${!_current_table}' (${BASH_SOURCE[1]:-} ${BASH_LINENO[0]:-})"
+    trace -sd 3 "Using current table: '${!_current_table}'"
 
     # for the proper behavior of this function change some global flags (to be restored before returning from the function)
     local _top=true  # is this the top header?
